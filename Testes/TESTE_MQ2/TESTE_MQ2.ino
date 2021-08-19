cdd262b0070e288c8644ea584e2b4b5db1020cb2
Yuri Marques Barboza <63083579+Yuri-m-b@users.noreply.github.com>
2021-08-19 20:07:15 -0300

    Add files via upload

---
+++ Testes/TESTE_MQ2/TESTE_MQ2.ino
@@ -0,0 +1,78 @@
+/*
+ * Aluno: Yuri Marques Barboza
+ * INSTITUTO FEDERAL DE SANTA CATARINA
+ * Disciplina: Projeto Integrador II
+ * Curso: Graduação em Engenharia Eletrônica
+ * Objetivo: Testar a função do teclado que mostra a leitura o valor lido pelo pino analógico do sensor de gás, e também a função que irá alertar no teclado serial caso o nível de gases esteja em um nível muito elevado
+ *  * 
+ *  *  *                  Arduino MEGA
+ *               -----------------
+ *              |                 |
+ *              |                 |         
+ *  PIR   <---  | PWM2            |        
+ *  LED1  <---  | PWM3            |
+ *  LED2  <---  | PWM4            |
+ *  LED3  <---  | PWM5        A0  | ---> MQ2        
+ *  LED4  <---  | PWM6            |        
+ *  LED5  <---  | PWM7        GND |->- Cabo GND
+ *  LED6  <---  | PWM8        VCC |->- Cabo VCC
+ *  SERVO <---  | PWM9            |        
+ *  MQ2   <---  | PWM10           |        
+ *  BUZZER<---  | PWM11           |    
+ *              |                 |  
+ *              |                 |  
+ *               -----------------
+ *                  USB   Fonte DC
+*/
+#define MQ2 10    // Pino digital do sensor de Gás
+#define PinA0 A0  // Pino analogico do sensor de Gás
+
+char incomingByte;
+// Sensor de Gás
+int leitura_sensor = 500;           //Definindo um valor limite (nível de gás normal)
+const unsigned long periodo_tarefa_2 = 500;
+unsigned long tempo_tarefa_2 = millis();
+ 
+void setup() {
+  Serial.begin(9600);          // opens serial port, sets data rate to 9600 bps
+  pinMode(MQ2, INPUT);         // PINO DIGITAL DO SENSOR MQ2
+}
+void tarefa_1() {                 // ARMAZENA SERIAL para incomingByte
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // say what you got:
+    Serial.print("Digito Recebido: ");
+    Serial.println(incomingByte);
+    
+    // Para MQ2    
+    if (incomingByte == 'a' || incomingByte == 'A')
+    {
+      int valor_analogico = analogRead(PinA0);
+      //int valor_analogico = analogRead(PinA0);
+      Serial.print("Nivel de concentracao de Gas: ");
+      Serial.println(valor_analogico);                      // Mostra no monitor serial o valor lido do pino analógico
+    }
+  }
+}
+void tarefa_2(unsigned long tempo_atual) {
+  if (tempo_atual - tempo_tarefa_2 > periodo_tarefa_2)
+  {
+    int valor_analogico = analogRead(PinA0);
+    tempo_tarefa_2 = tempo_atual;
+    if (valor_analogico > leitura_sensor)                   //Se o valor lido no pino analógico for maior que o valor limite, imprime a mensagem de aviso
+    {
+      Serial.println("FUMACA DETECTADA");
+      Serial.println("PERIGO!!!!");
+    }
+  }
+}
+ 
+void loop(){
+  unsigned long meu_tempo_atual = millis();
+
+  tarefa_1();
+  tarefa_2(meu_tempo_atual);
+}
